aed91b401024d671f0f96f960f6fdf4517f246e5
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-07 05:11:09 +0000

    Add height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,13 +7,18 @@
 size_t findMax(size_t right, size_t left);
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t heightTrue;
+	size_t height = 0, right = 0, left = 0;
+
 	if (tree == NULL)
 		return (0);
+	if (tree->left != NULL)
+		left = binary_tree_height(tree->left) + 1;
+	if (tree->right != NULL)
+		right = binary_tree_height(tree->right) + 1;
+	
+	height = findMax(right, left);
 
-	heightTrue = (findMax(binary_tree_height(tree->left), binary_tree_height(tree->right)) + 1);
-
-	return (heightTrue);
+	return (height);
 }
 /**
  * findMax - find heighest hight to return
@@ -23,7 +28,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 size_t findMax(size_t right, size_t left)
 {
-	if (right >= left)
+	if (right > left)
 		return (right);
 	else
 		return (left);
