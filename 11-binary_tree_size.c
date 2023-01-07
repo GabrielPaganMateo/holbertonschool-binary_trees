6eadaabe73ece18825e4b4772c3964c9a2989773
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-07 16:37:14 +0000

    Fix size function

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -14,9 +14,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		size += 1;
 
 	if (tree->left)
-		binary_tree_size(tree->left);
+		size += binary_tree_size(tree->left);
 	if (tree->right)
-		binary_tree_size(tree->right);
+		size += binary_tree_size(tree->right);
 
 	return (size);
 }
