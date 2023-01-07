b971bfa5936f1a8f90d80b6e08414655ad4803d2
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-07 05:23:05 +0000

    Clean look to 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,9 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL)
+	if (tree->left)
 		left = binary_tree_height(tree->left) + 1;
-	if (tree->right != NULL)
+	if (tree->right)
 		right = binary_tree_height(tree->right) + 1;
 
 	height = findMax(right, left);
