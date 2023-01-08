8ec49021af80563afad2a50dba30df2e37a4025e
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-08 21:17:58 +0000

    balance ?

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 size_t findMax(size_t right, size_t left);
+size_t tree_height(const binary_tree_t *tree);
 /**
  * binary_tree_balance - measures the balance factor of binary tree
  * @tree: pointer to node measured
@@ -8,23 +9,45 @@ size_t findMax(size_t right, size_t left);
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int heightL = 0, heightR = 0;
-	static int balance = 0;
-
-	if (tree->right == NULL && tree->left == NULL)
-		return (0);
+	int balance = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-	heightL += binary_tree_balance(tree->left) + 1;
-
-	if (tree->right)
-	heightR += binary_tree_balance(tree->right) + 1;
+	heightL = tree_height(tree->left);
+	heightR = tree_height(tree->right);
 
 	printf("heightL:%i heightR:%i\n", heightL, heightR);
 
 	balance = heightL - heightR;
 	return (balance);
 
+}
+size_t tree_height(const binary_tree_t *tree)
+{
+	size_t height = 0, right = 0, left = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		left = tree_height(tree->left) + 1;
+	if (tree->right)
+		right = tree_height(tree->right) + 1;
+
+	height = findMax(right, left);
+
+	return (height);
+}
+/**
+ * findMax - find heighest hight to return
+ * @right: right subtree
+ * @left: left subtree
+ * Return: tallest height
+ */
+size_t findMax(size_t right, size_t left)
+{
+	if (right > left)
+		return (right);
+	else
+		return (left);
 }
