0097ca3a12c51c2f633bfc8813391d5b5c6dce85
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-09 14:44:33 +0000

    Finished!

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,4 +1,4 @@
-#include "binary.trees.h"
+#include "binary_trees.h"
 /**
  * binary_tree_uncle - finds uncle of node
  * @node: nephew
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL)
 		return (NULL);
 
-	if (node->parent == NULL)
+	if (node->parent->parent == NULL)
 		return (NULL);
 
 	if (node->parent == node->parent->parent->left)
